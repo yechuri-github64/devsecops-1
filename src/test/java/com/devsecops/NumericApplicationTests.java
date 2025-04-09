2c64685bdba142fd62a0bc028036340417293502
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2025-04-09 10:09:20 +0530

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -42,7 +42,7 @@ public class NumericApplicationTests {
 
     @Test
     public void greaterThanFiftyMessage() throws Exception {
-        this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
     }
     
